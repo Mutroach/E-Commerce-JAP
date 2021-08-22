42653bd136293bb001c3b8590b197c1d3119338f
Mutroach <genolet.lucas@gmail.com>
2021-08-22 11:45:59 -0300

    cambios al login y ajustes varios

--- js/login.js
+++ js/login.js
@@ -1,12 +1,11 @@
 document.addEventListener("DOMContentLoaded", function(e){ 
 
     document.getElementById("ingresar").onclick = function(){
-        let email = document.getElementById("email").value.length;
-        let contraseña = document.getElementById("contraseña").value.length; 
+        let email = document.getElementById("email").value;
+        let contraseña = document.getElementById("contraseña").value; 
 
         if (email == "" || contraseña == "") {
-            document.getElementById("faltanDatos").innerHTML = "Debes ingresar tus datos gil";
-            console.log(email);
+            document.getElementById("faltanDatos").innerHTML = "Debes ingresar tu usuario y contraseña";
         }
         else {
             window.location.href = "./front-page.html";
