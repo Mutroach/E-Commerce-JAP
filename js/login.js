e8892275385341f6da8a325954989d63fccf47a4
Mutroach <genolet.lucas@gmail.com>
2021-08-28 19:40:04 -0300

    cambios varios

--- js/login.js
+++ js/login.js
@@ -1,6 +1,4 @@
-document.addEventListener("DOMContentLoaded", function(e){ 
-
-    document.getElementById("ingresar").onclick = function(){
+document.getElementById("ingresar").onclick = function(){
         let email = document.getElementById("email").value;
         let contraseña = document.getElementById("contraseña").value; 
 
@@ -10,5 +8,6 @@ document.addEventListener("DOMContentLoaded", function(e){
         else {
             window.location.href = "./front-page.html";
         }
-    }
-});
+
+        localStorage.setItem("email", email);
+    };
--- js/products.js
+++ js/products.js
@@ -1,30 +1,50 @@
-document.addEventListener("DOMContentLoaded", function(e){
+let precioMinimo = "";
+let precioMaximo = "";
+let jsonData = "";
 
 fetch(PRODUCTS_URL)
     .then(response => response.json())
     .then(data => {
+        jsonData = data;
+    })
+
+function content(){
     let htmlContent = "";
 
-        for(let i = 0; i < data.length; i++){
-            let product = data[i];
+    for(let i = 0; i < jsonData.length; i++){
+        let product = jsonData[i];
 
-            htmlContent += `
-            <div class="list-group-item list-group-item-action">
-                <div class="row">
-                    <div class="col-3">
-                        <img src="` + product.imgSrc + `" alt="` + product.description + `" class="img-thumbnail">
-                    </div>
-                    <div class="col">
-                        <div class="d-flex w-100 justify-content-between">
-                            <h4 class="mb-1">`+ product.name +`</h4>
-                        
-                            <small class="text-muted">` + product.currency + ` `+ product.cost +  `</small>
-                        </div>
-                        <div><h5>` + product.description + `</h5></div>
-                    </div>
+        htmlContent = 
+        `<div class="list-group-item list-group-item-action">
+            <div class="row">
+                <div class="col-3">
+                    <img src="` + product.imgSrc + `" alt="` + product.description + `" class="img-thumbnail">
                 </div>
-            </div>`
-        document.getElementById("productos").innerHTML = htmlContent;
+                <div class="col">
+                    <div class="d-flex w-100 justify-content-between">
+                        <h4 class="mb-1">`+ product.name +`</h4>
+                    
+                        <small class="text-muted">` + product.currency + ` `+ product.cost +  `</small>
+                    </div>
+                    <div><h5>` + product.description + `</h5></div>
+                </div>
+            </div>
+        </div>`;
+        
+        if(product.cost >= precioMinimo && product.cost <= precioMaximo) {
+            document.getElementById("productos").innerHTML += htmlContent;
         }
-    })
-});
+        else if(precioMinimo === "" && precioMaximo === ""){
+            document.getElementById("productos").innerHTML += htmlContent;
+        }
+    }
+    console.log(precioMinimo);
+    console.log(precioMaximo);
+}
+
+document.getElementById("filtroPrecioBtn").onclick = function() {
+    document.getElementById("productos").innerHTML = "";
+    precioMinimo = document.getElementById("minimo").value;
+    precioMaximo = document.getElementById("maximo").value;
+    content();
+}
