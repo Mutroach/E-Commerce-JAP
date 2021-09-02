21d542df7023319fa7932aee4dd138fd681bcddf
Mutroach <genolet.lucas@gmail.com>
2021-09-02 14:45:54 -0300

    Nuevos filtros y ajustes

--- js/products.js
+++ js/products.js
@@ -1,11 +1,15 @@
 let precioMinimo = "";
 let precioMaximo = "";
 let jsonData = "";
+function sortPrecioAscendente(){ jsonData.sort((item1, item2) => item1.cost - item2.cost)};
+function sortPrecioDescendente(){ jsonData.sort((item1, item2) => item2.cost - item1.cost)};
+function sortRelevancia(){ jsonData.sort((item1, item2) => item2.soldCount - item1.soldCount)};
 
 fetch(PRODUCTS_URL)
     .then(response => response.json())
     .then(data => {
         jsonData = data;
+        sortPrecioAscendente();
         content();
     })
 
@@ -25,26 +29,43 @@ function content(){
                     <div class="d-flex w-100 justify-content-between">
                         <h4 class="mb-1">`+ product.name +`</h4>
                     
-                        <small class="text-muted">` + product.currency + ` `+ product.cost +  `</small>
+                        <small class="text-muted"> Precio: ` + product.currency + ` `+ product.cost +  ` <br> Cantidad de vendidos: ` + product.soldCount + `</small>
                     </div>
                     <div><h5>` + product.description + `</h5></div>
                 </div>
             </div>
         </div>`;
         
-        if(product.cost >= precioMinimo && product.cost <= precioMaximo) {
+        if(product.cost >= precioMinimo && product.cost <= precioMaximo || product.cost >= precioMinimo && precioMaximo == "" || product.cost <= precioMaximo && precioMinimo == "") {
             document.getElementById("productos").innerHTML += htmlContent;
         }
         else if(precioMinimo == "" && precioMaximo == ""){
             document.getElementById("productos").innerHTML += htmlContent;
         }
     }
-    console.log("buenas")
 }
 
 document.getElementById("filtroPrecioBtn").onclick = function() {
     document.getElementById("productos").innerHTML = "";
     precioMinimo = document.getElementById("minimo").value;
     precioMaximo = document.getElementById("maximo").value;
+    content();
+}
+
+document.getElementById("filtroAscendenteBtn").onclick = function() {
+    document.getElementById("productos").innerHTML = "";
+    sortPrecioAscendente();
+    content();
+}
+
+document.getElementById("filtroDescendenteBtn").onclick = function() {
+    document.getElementById("productos").innerHTML = "";
+    sortPrecioDescendente();
+    content();
+}
+
+document.getElementById("filtroRelevanciaBtn").onclick = function() {
+    document.getElementById("productos").innerHTML = "";
+    sortRelevancia();
     content();
 }
