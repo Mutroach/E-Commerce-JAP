dd485e40820b1a076c97cf7b11ef4b82cb3b6bf9
Mutroach <genolet.lucas@gmail.com>
2021-09-03 02:30:59 -0300

    web responsive

--- js/login.js
+++ js/login.js
@@ -1,6 +1,13 @@
+
+
 document.getElementById("ingresar").onclick = function(){
         let email = document.getElementById("email").value;
         let contraseña = document.getElementById("contraseña").value; 
+
+        let hoy = new Date();
+        let fecha = hoy.getDate() + "/" + (hoy.getMonth()+1) + "/" + hoy.getFullYear();
+        let hora = hoy.getHours() + ":" + hoy.getMinutes();
+        let fechaYhora = fecha + " " + hora;
 
         if (email == "" || contraseña == "") {
             document.getElementById("faltanDatos").innerHTML = "Debes ingresar tu usuario y contraseña";
@@ -10,4 +17,5 @@ document.getElementById("ingresar").onclick = function(){
         }
 
         localStorage.setItem("email", email);
+        localStorage.setItem("fechaYhora", fechaYhora);
     };
