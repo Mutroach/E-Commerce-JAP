50666d2243315072f51f0467a3eb503c03fac150
Mutroach <genolet.lucas@gmail.com>
2021-09-10 01:10:01 -0300

    Calificación con estrellas

--- js/product-info.js
+++ js/product-info.js
@@ -30,19 +30,38 @@ fetch(PRODUCT_INFO_COMMENTS_URL)
         }
 })
 
-
 document.getElementById("enviarComentario").onclick = function() {
     let hoy = new Date();
     let fecha = hoy.getFullYear() + "-" + (hoy.getMonth()+1) + "-" + hoy.getDate();
     let hora = hoy.getHours() + ":" + hoy.getMinutes() + ":" + hoy.getSeconds();
     let fechaYhora = fecha + " " + hora;
+    var starsChecked = '<span class="fa fa-star checked"></span>';
+    var starsUnchecked = '<span class="fa fa-star"></span>';
 
-    document.getElementById("comentarios").innerHTML += `
+    if(document.getElementById("comentarioRealizado").value != "" && valorEstrella != "") {
+        document.getElementById("comentarios").innerHTML += `
                 <div> <b>Usuario: ` + localStorage.getItem("email") + ` </b></div>
                 <div> "` + document.getElementById("comentarioRealizado").value + `" </div>
-                <div> Puntuación: te la debo </div>
+                <div> Puntuación: ` + starsChecked.repeat(valorEstrella) + starsUnchecked.repeat(5-valorEstrella) + `</div>
                 <div> Comentario realizado en ` + fechaYhora + `</div>
-                <br>`
+                <br>`;
 
-    document.getElementById("comentarioRealizado").value = ""
+    document.getElementById("comentarioRealizado").value = "";
+    document.getElementById("error").innerHTML = "";
+    document.getElementsByName("estrellas")[0].checked = false;
+    document.getElementsByName("estrellas")[1].checked = false;
+    document.getElementsByName("estrellas")[2].checked = false;
+    document.getElementsByName("estrellas")[3].checked = false;
+    document.getElementsByName("estrellas")[4].checked = false;
+    valorEstrella = "";
+    }
+    else {
+        document.getElementById("error").innerHTML = `<p style="color:red;">Debes ingresar un comentario y seleccionar una puntuación</p>`;
+    }
+}
+
+let valorEstrella = "";
+
+function puntuacionEstrella(estrella) {
+    valorEstrella = estrella;
 }
